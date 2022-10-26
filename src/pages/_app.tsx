c832b5978f4cce7b05922f9d1d34c2331a141784
ki0i0ro0 <ki0i0ro0@gmail.com>
2022-10-26 23:17:27 +0900

    [update] format prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import '../common/styles/ress.css'
-import '../common/styles/style.css'
+import "../common/styles/ress.css";
+import "../common/styles/style.css";
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
