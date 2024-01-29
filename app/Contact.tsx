99f8f14dd99ae0065b138d5b04c7be76f1256e30
ki0i0ro0 <ki0i0ro0@gmail.com>
2024-01-29 19:38:10 +0900

    コンタクト Fixes #7

--- app/Contact.tsx
+++ app/Contact.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const Contact = () => (
   <section id="contact">
     <h2>コンタクト</h2>
-    <p>X(旧：twitter)のDMからご連絡ください。</p>
+    <p>
+      X(旧：twitter)のDMからご連絡ください。
+      <a href="https://twitter.com/ki0i0ro0">@ki0i0ro0</a>
+    </p>
   </section>
 );
 
