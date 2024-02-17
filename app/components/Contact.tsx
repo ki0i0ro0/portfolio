d4b257b06411461ec45f3ee93f22cadf2d8e9945
ki0i0ro0 <ki0i0ro0@gmail.com>
2024-02-17 15:49:51 +0900

    Refactor Contact component to use Material-UI Typography and Link components

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,13 +1,28 @@
+import { Link, Typography } from "@mui/material";
 import React from "react";
 
 function Contact() {
   return (
     <section id="contact">
-      <h2>コンタクト</h2>
-      <p>
+      <Typography
+        variant="h5"
+        component="h2"
+        sx={{ fontWeight: "bold" }}
+        gutterBottom
+      >
+        コンタクト
+      </Typography>
+      <Typography variant="body1">
         X(旧：twitter)のDMからご連絡ください。
-        <a href="https://twitter.com/ki0i0ro0">@ki0i0ro0</a>
-      </p>
+        <Link
+          href="https://twitter.com/ki0i0ro0"
+          underline="hover"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @ki0i0ro0
+        </Link>
+      </Typography>
     </section>
   );
 }
