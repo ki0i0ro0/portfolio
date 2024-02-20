a4bf5c31b7147e97eb882086f180ad4f8cc75656
ki0i0ro0 <kensaku.matsumoto@x-factory.jp>
2024-02-20 20:19:22 +0900

    Remove unused CSS styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
